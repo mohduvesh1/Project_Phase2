6ff9139edd78a695c48107482af6962b61843ddb
TheUzair <mohujer90@gmail.com>
2025-02-13 12:53:59 +0530

    initial commit of phase2 project

---
+++ Project_Phase2/app.js
@@ -0,0 +1,9 @@
+let h2= document.querySelector('h2');
+let button= document.querySelector('button');
+button.addEventListener('click',function(){
+    h2.style.color='red';
+    h2.style.backgroundColor='black';
+    h2.style.fontSize='50px';
+    h2.style.padding='10px';
+    h2.style.borderRadius='10px';
+});
