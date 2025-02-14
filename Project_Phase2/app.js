f130956ff7b80ac5bb4baa9812fdc67f5ce808f9
TheUzair <mohujer90@gmail.com>
2025-02-14 12:10:16 +0530

    added a button

--- Project_Phase2/app.js
+++ Project_Phase2/app.js
@@ -1,7 +1,1 @@
-let h2 = document.querySelector("h2");
-let button = document.querySelector("button");
-button.addEventListener("click", function () {
-    h2.style.color = "red";
-    h2.style.backgroundColor = "black";
-    h2.style.fontSize = "50px";
-});
+// add new feature - button
